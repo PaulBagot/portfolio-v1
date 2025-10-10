bdeb1c8bf918dda876c12e1b54cda0bd61b93e04
0LrNx <FISSSSSS@proton.me>
2025-10-10 16:48:01 +0200

    feat: add socials networks

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
-/* import { Navbar } from './components/navbar' */
 
 
 
@@ -10,10 +9,7 @@ createRoot(document.getElementById('root')!).render(
     <div className='w-full h-full bg-neutral-900'>
       <div className='w-3/4 h-full mx-auto bg-neutral-50'>
         <App />
-        {/* <div className="fixed bottom-0 left-0 w-full z-50 hidden md:block">
-          <Navbar />
-        </div> */}
       </div>
     </div>
-  </StrictMode>,
+  </StrictMode>
 )
--- src/sections/contact.tsx
+++ src/sections/contact.tsx
@@ -1,33 +1,68 @@
 import BlurFade from "@/components/magicui/blur-fade";
 import { DATA } from "@/data/resume";
+import { SocialTooltip } from '@/components/ui/social-media';
+import type { SocialItem } from '@/components/ui/social-media';
 
+const socialLinks: SocialItem[] = [
+    {
+        href: "#",
+        ariaLabel: "Shopify",
+        tooltip: "Shopify",
+        color: "#64943E",
+        svgUrl: "https://svgl.app/library/shopify.svg",
+    },
+    {
+        href: "#",
+        ariaLabel: "Edge",
+        tooltip: "Edge",
+        color: "#0078D7",
+        svgUrl: "https://svgl.app/library/edge.svg",
+    },
+    {
+        href: "#",
+        ariaLabel: "Facebook",
+        tooltip: "Facebook",
+        color: "#3b5998",
+        svgUrl: "https://svgl.app/library/facebook.svg",
+    },
+    {
+        href: "#",
+        ariaLabel: "Telegram",
+        tooltip: "Telegram",
+        color: "#0088cc",
+        svgUrl: "https://svgl.app/library/telegram.svg",
+    },
+];
 
 export function Contact() {
     return (
-        <section id="contact">
-            <div className="grid items-center justify-center gap-4 px-4 text-center md:px-6 w-full py-12">
-                <BlurFade delay={0.04 * 16}>
-                    <div className="space-y-3">
-                        <div className="inline-block rounded-lg bg-foreground text-background px-3 py-1 text-sm">
-                            Contact
+        <>
+            <section id="contact">
+                <div className="grid items-center justify-center gap-4 px-4 text-center md:px-6 w-full py-12">
+                    <BlurFade delay={0.04 * 16}>
+                        <div className="space-y-3">
+                            <div className="inline-block rounded-lg bg-foreground text-background px-3 py-1 text-sm">
+                                Contact
+                            </div>
+                            <h2 className="text-3xl font-bold tracking-tighter sm:text-5xl">
+                                Get in Touch
+                            </h2>
+                            <p className="mx-auto max-w-[600px] text-muted-foreground md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed">
+                                Want to chat? Just shoot me a dm{" "}
+                                <a
+                                    href={DATA.contact.social.X.url}
+                                    className="text-blue-500 hover:underline"
+                                >
+                                    with a direct question on twitter
+                                </a>{" "}
+                                and I&apos;ll respond whenever I can. I will ignore all
+                                soliciting.
+                            </p>
                         </div>
-                        <h2 className="text-3xl font-bold tracking-tighter sm:text-5xl">
-                            Get in Touch
-                        </h2>
-                        <p className="mx-auto max-w-[600px] text-muted-foreground md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed">
-                            Want to chat? Just shoot me a dm{" "}
-                            <a
-                                href={DATA.contact.social.X.url}
-                                className="text-blue-500 hover:underline"
-                            >
-                                with a direct question on twitter
-                            </a>{" "}
-                            and I&apos;ll respond whenever I can. I will ignore all
-                            soliciting.
-                        </p>
-                    </div>
-                </BlurFade>
-            </div>
-        </section>
+                    </BlurFade>
+                </div>
+            </section>
+            <SocialTooltip className="mb-12 -mt-20" items={socialLinks} />
+        </>
     )
 }
